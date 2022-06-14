b3399e0a0df7b19f1de9c4ff569a03c6ca82cde0
Affluent Jacob <affluentsoftware.innovations@gmail.com>
2022-06-14 18:27:34 +0100

    done with the development of node.js app

---
+++ app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const connectDB = require('./config/db')
+
+
+// load config
+dotenv.config({path: './config/config.env'})
+
+// passport config
+require('./config/passport')(passport)
+
+// connect the DB
+connectDB()
+
+// initialize express app
+const app = express()
+
+// body parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// method override
+app.use(methodOverride(function(req, res){
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+// loggin
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+// Handlebars Helpers
+const {FormatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
+// Handlebars
+app.engine('.hbs', engine({helpers:{
+FormatDate,
+stripTags,
+truncate,
+editIcon,
+select,
+}, 
+defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// session middleware
+ app.use(session({
+     secret: 'keyboard cat',
+     resave: false,
+     saveUninitialized: false,
+     store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+      })
+ }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// set global var
+app.use(function (req, res, next){
+    res.locals.user = req.user || null
+    next()
+})
+
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
